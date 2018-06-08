1f069ec56c7b9b6b3a6816fc4d652af4d3d9dbf3
Gunes <elifedagune4@gmail.com>
2018-06-09 00:48:42 +0300

    Kategori

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
+class Kategori(models.Model):
+     kategori_adi = models.CharField(max_length=100) 
 
 class Post(models.Model):
+    kategori = models.ForeignKey( Kategori, on_delete=models.CASCADE, null=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -17,4 +20,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
